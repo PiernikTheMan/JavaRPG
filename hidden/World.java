273d69bacfd9119358cf31fd6213fa5b1532c6c7
piernik <wojtek@surowka.com>
2018-01-31 23:36:38 +0100

    finding and copying story to memory

--- hidden/World.java
+++ hidden/World.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 class World{
     int Stage=0;
     
@@ -21,13 +22,13 @@ class World{
                 }
             }
         stageReader.close();
-        System.out.println(Stage);
+        System.out.println("stage is "+Stage);
         if(Stage==0){
             try(FileOutputStream fileOut = new FileOutputStream("./hidden/config.discouragingextension")){
                 String inputStr = inputBuffer.toString();
-                System.out.println(inputStr);
                 inputStr=inputStr.replace("Stage 0","Stage 1");
-                System.out.println(inputStr);
+                Stage=1;
+                this.pushThePlot(Stage);
                 fileOut.write(inputStr.getBytes());
             }
             catch(Exception e){
@@ -45,14 +46,43 @@ class World{
     }
     
     private static void pushThePlot(int Stage){
-        try(BufferedReader story = new BufferedReader(new FileReader("./hidden/Story.dat")); BufferedReader player = new BufferedReader(new FileReader("Player.java"))){
-            ArrayList lines =new ArrayList();
+        try(BufferedReader story = new BufferedReader(new FileReader("./hidden/Story.dat"))/*; BufferedReader player = new BufferedReader(new FileReader("Player.java"))*/){
+            // try to get a lines from story 
+            ArrayList<String> lines =new ArrayList<String>();
+            String line=null;
+            boolean start=false;
+            System.out.println("Stage "+Integer.toString(Stage+1)+"*");
+            String temp="Stage "+Integer.toString(Stage);
+            Pattern whatToAdd =Pattern.compile(temp);
+            temp="Stage "+Integer.toString(Stage+1);
+            Pattern endStage =Pattern.compile(temp);
+            Matcher matcher=null; 
+            while((line=story.readLine())!=null){
+                if(start){
+                    matcher=endStage.matcher(line);
+                    if(matcher.find()){
+                        break;
+                    }
+                    lines.add(line);
+                }
+                else{
+                    matcher=whatToAdd.matcher(line);
+                    if(matcher.find()){
+                        start=true;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(lines.toArray()));
         }
         catch(FileNotFoundException e){
             System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
         }
         catch(IOException e){
             System.out.println("You shuld not be able to see this. Contact the gamme developer. error code IOException");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e); 
         }
     }
 }
