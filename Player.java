0a6be59fb0da860717ecd352f64cdcf2df4158e3
piernik <wojtek@surowka.com>
2018-02-02 00:20:08 +0100

    game is done just add a hint generator

--- Player.java
+++ Player.java
@@ -1,9 +1,7 @@
 class Player extends Human{
     public static void main(String[] args){
-        //Story:In beginning there was the word, but the uniwerse was emply so shortly after The God "created" the "World"
-        //Story:But isn't The God just a creation of a man? Does it makes the world a creation of a man?
-        //Story:Did the "Player" "created" the "World"??
-        World mine=new World();
+        //Narrator:I guess you can see
+        World mine=new World("my ass");
         System.out.println(see());
     }
 }
--- hidden/World.java
+++ hidden/World.java
@@ -5,53 +5,28 @@ class World{
     int Stage=0;
     
     World(){
-        try{
-            BufferedReader stageReader = new BufferedReader(new FileReader("./hidden/config.discouragingextension")); 
-            String line;
-            StringBuffer inputBuffer = new StringBuffer();
-            while((line=stageReader.readLine())!=null){
-                inputBuffer.append(line);
-                inputBuffer.append('\n');
-                Scanner Stagelooker =new Scanner(line);
-                String template= "Stage";
-                while(Stagelooker.hasNext()){
-                    if(template.equals(Stagelooker.next())){
-                        Stagelooker.hasNext();
-                        Stage=Integer.parseInt(Stagelooker.next());
-                    }
-                }
-            }
-        stageReader.close();
-        System.out.println("stage is "+Stage);
-        if(Stage==0){
-            try(FileOutputStream fileOut = new FileOutputStream("./hidden/config.discouragingextension")){
-                String inputStr = inputBuffer.toString();
-                inputStr=inputStr.replace("Stage 0","Stage 1");
-                Stage=1;
-                this.pushThePlot(Stage);
-                fileOut.write(inputStr.getBytes());
-            }
-            catch(Exception e){
-                System.out.println("Error inside the writein to config. You shuld not be able to see this. Contact the gamme developer."); 
+        Stage=getStage();
+        if (Stage==0){
+            changeStageTo(1);
+            pushThePlot(1);
+        }
+    }
+    World(String key){
+        this();
+        if(key.equals("calm down")){
+            if(Stage<2){
+                changeStageTo(2);
+                pushThePlot(2);
             }
         }
-        System.out.println("you did it!");
-        }
-    catch(FileNotFoundException e){
-        System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
     }
-    catch(Exception e){
-            System.out.println("You shuld not be able to see this. Contact the gamme developer."); 
-    }
-    }
-    
     private static void pushThePlot(int Stage){
-        try(BufferedReader story = new BufferedReader(new FileReader("./hidden/Story.dat"))/*; BufferedReader player = new BufferedReader(new FileReader("Player.java"))*/){
+        ArrayList<String> lines=null;
+        try(    BufferedReader story = new BufferedReader(new FileReader("./hidden/Story.dat")); BufferedReader player = new BufferedReader(new FileReader("Player.java"))  ){
             // try to get a lines from story 
-            ArrayList<String> lines =new ArrayList<String>();
+            lines =new ArrayList<String>();
             String line=null;
             boolean start=false;
-            System.out.println("Stage "+Integer.toString(Stage+1)+"*");
             String temp="Stage "+Integer.toString(Stage);
             Pattern whatToAdd =Pattern.compile(temp);
             temp="Stage "+Integer.toString(Stage+1);
@@ -72,7 +47,64 @@ class World{
                     }
                 }
             }
-            System.out.println(Arrays.toString(lines.toArray()));
+        }
+        catch(FileNotFoundException e){
+            System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
+        }
+        catch(IOException e){
+            System.out.println("You shuld not be able to see this. Contact the gamme developer. error code IOException");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e); 
+        }
+        // lines from the storry are in ArrayList lines 
+        // put a code into a string with changes
+        try{
+            BufferedReader stageReader = new BufferedReader(new FileReader("Player.java"));
+            String line;
+            boolean isStoryWritten=false;
+            StringBuffer inputBuffer = new StringBuffer();
+            String temp="Narrator:";
+            if (Stage==1)    temp="Story:";
+            Pattern storyPattern =Pattern.compile("Story:");
+            Pattern narratorPattern =Pattern.compile("Narrator:");
+            Matcher matcher=null; 
+            Pattern mainfun = Pattern.compile("public static void main");
+            Matcher storyMatcher=null;
+            while((line=stageReader.readLine())!=null){
+                matcher = narratorPattern.matcher(line);
+                storyMatcher = mainfun.matcher(line);
+                if(matcher.find()){
+                    continue;
+                }
+                matcher = storyPattern.matcher(line);
+                if(matcher.find()){
+                    continue;
+                }
+                if(storyMatcher.find()){
+                inputBuffer.append(line);
+                inputBuffer.append('\n');
+                Iterator<String> poStory = lines.iterator();
+                while(poStory.hasNext()){
+                    inputBuffer.append("        ");
+                    inputBuffer.append(poStory.next());
+                    inputBuffer.append('\n');
+                }
+                continue;
+                }
+                inputBuffer.append(line);
+                inputBuffer.append('\n');
+                }
+                stageReader.close();
+                // nadpisanie nowego pliku 
+                try(FileOutputStream fileOut = new FileOutputStream("Player.java")){
+                    String inputStr = inputBuffer.toString();
+                    fileOut.write(inputStr.getBytes());
+                }
+                catch(Exception e){
+                    System.out.println("Error inside the writein to config. You shuld not be able to see this. Contact the gamme developer."); 
+                }      
         }
         catch(FileNotFoundException e){
             System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
@@ -85,4 +117,70 @@ class World{
             System.out.println(e); 
         }
     }
+    
+    private static int getStage(){
+        try{
+            BufferedReader stageReader = new BufferedReader(new FileReader("./hidden/config.discouragingextension")); 
+            String line;
+            while((line=stageReader.readLine())!=null){
+                Scanner Stagelooker =new Scanner(line);
+                String template= "Stage";
+                while(Stagelooker.hasNext()){
+                    if(template.equals(Stagelooker.next())){
+                        Stagelooker.hasNext();
+                        return Integer.parseInt(Stagelooker.next());
+                    }
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
+            return -1;
+        }
+        catch(Exception e){
+            System.out.println("You shuld not be able to see this. Contact the gamme developer."); 
+            return -1;
+        }
+    return -2;
+    }
+    
+    private static void changeStageTo(int target){
+        int stagelocal=0;
+        try{
+             //read the config file 
+            BufferedReader stageReader = new BufferedReader(new FileReader("./hidden/config.discouragingextension")); 
+            String line;
+            StringBuffer inputBuffer = new StringBuffer();
+            while((line=stageReader.readLine())!=null){
+                inputBuffer.append(line);
+                inputBuffer.append('\n');
+                Scanner Stagelooker =new Scanner(line);
+                String template= "Stage";
+                while(Stagelooker.hasNext()){
+                    if(template.equals(Stagelooker.next())){
+                        Stagelooker.hasNext();
+                        stagelocal=Integer.parseInt(Stagelooker.next());
+                    }
+                }
+            }
+        stageReader.close();
+        //write the config file 
+            try(FileOutputStream fileOut = new FileOutputStream("./hidden/config.discouragingextension")){
+                String inputStr = inputBuffer.toString();
+                inputStr=inputStr.replace("Stage "+ Integer.toString(stagelocal),"Stage "+ Integer.toString(target));
+                fileOut.write(inputStr.getBytes());
+            }
+            catch(Exception e){
+                System.out.println("Error inside the writein to config. You shuld not be able to see this. Contact the gamme developer."); 
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("You shuld not be able to see this. Contact the gamme developer. error code filenotFoundException");
+        }
+        catch(Exception e){
+                System.out.println("You shuld not be able to see this. Contact the gamme developer."); 
+        }
+    }
+    
+    
 }
